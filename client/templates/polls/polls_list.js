49e2f921911a78d951585bf791bdac417881ea50
davidleroy-js <davidleroy.js@gmail.com>
2017-03-12 20:06:27 -0700

    connected polls_list.js to mongo collection

--- client/templates/polls/polls_list.js
+++ client/templates/polls/polls_list.js
@@ -1,18 +1,5 @@
-var pollsData = [
-    {
-        title: 'Cats or Dogs?',
-        author: 'Dave'
-    },
-    {
-        title: 'Favorite Burger?',
-        author: 'Dave'
-    },
-    {
-        title: 'Android or iOS?',
-        author: 'Dave'
-    }
-];
-
 Template.pollsList.helpers({
-    polls: pollsData
+    polls: function(){
+        return Polls.find();
+    }
 });
