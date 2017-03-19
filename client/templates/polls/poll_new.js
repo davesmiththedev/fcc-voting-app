7e4bffa41afacc38e5bc45889d188cb8c465bd0f
davidleroy-js <davidleroy.js@gmail.com>
2017-03-19 10:45:16 -0700

    add additional fields to poll options

--- client/templates/polls/poll_new.js
+++ client/templates/polls/poll_new.js
@@ -2,16 +2,25 @@ Template.pollNew.events({
     'submit form': function(e){
         e.preventDefault();
 
-        var optionsList = [];
+        var user = Meteor.user();
+        
         var optionsFromForm = document.getElementsByClassName("options");
+        var optionsList = [];
 
         [].forEach.call(optionsFromForm, (optionItem)=>{
-            optionsList.push({name: optionItem.value, votes: 0});
+            optionsList.push({
+                name: optionItem.value, 
+                votes: 0,
+                author: user.username,
+                created: new Date()
+            });
         });
 
         var poll = {
             title: $(e.target).find('[name=title]').val(),
-            options: optionsList
+            options: optionsList,
+            author: user.username,
+            created: new Date()
         };
 
         Meteor.call('pollInsert', poll, function(error, result){
