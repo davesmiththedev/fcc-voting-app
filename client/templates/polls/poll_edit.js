7a425129aac2596d2de781d3f507492c37edcb3b
davidleroy-js <davidleroy.js@gmail.com>
2017-04-03 14:23:56 -0700

    Add new option feature to poll edit page

--- client/templates/polls/poll_edit.js
+++ client/templates/polls/poll_edit.js
@@ -1,61 +1,95 @@
-$(function()
-{
-    $(document).on('click', '.btn-add', function(e)
-    {
-        e.preventDefault();
+import { Session } from 'meteor/session'
 
-        var controlForm = $('#options'),
-            currentEntry = $(this).parents('.entry:first'),
-            newEntry = $(currentEntry.clone()).appendTo(controlForm);
-
-        newEntry.find('input').val('');
-        controlForm.find('.entry:not(:last) .btn-add')
-            .removeClass('btn-add').addClass('btn-remove')
-            .removeClass('btn-success').addClass('btn-danger')
-            .html('<span class="glyphicon glyphicon-minus"></span>');
-    })
+//Set id for global temaplte use
+let urlParts = location.href.split('/');
+Session.set({
+    id: urlParts.pop()
 });
 
-
 Template.pollEdit.events({
-    'click .btn-remove': function(e)
-    {
-		$(this).parents('.entry:first').remove();
-
-		e.preventDefault();
-		return false;
-	},
-    
-    'submit form': function(e){
+    'change #title': function(e){
         e.preventDefault();
 
-        var user = Meteor.user();
-
-        console.log("Submit");
-        
-        // var pollOptions = document.getElementsByClassName("options");
-        // var optionsList = [];
-
-        // [].forEach.call(pollOptions, (optionItem)=>{
-        //     optionsList.push({
-        //         name: optionItem.value, 
-        //         votes: 0,
-        //         author: user.username,
-        //         created: new Date()
-        //     });
-        // });
-
         var pollUpdates = {
-            _id: this._id,
-            title: $(e.target).find('[name=title]').val(),
-            // options: optionsList,
+            _id: Session.get('id'),
+            title: $(e.target).val(),
         };
 
-        Meteor.call('pollEdit', pollUpdates, function(error, result){
+        Meteor.call('pollEditTitle', pollUpdates, function(error, result){
             if(error){
                 return alert(error.reason);
             };
         });
+
+        return false;
+    },
+
+    'focus .options': function(e){
+        e.preventDefault();
+        Session.set({
+            oldName: $(e.target).val()
+        });
+    },
+
+    'change .options': function(e){
+        e.preventDefault();
+        
+        let pollOption = {
+            _id: Session.get('id'),
+            oldName: Session.get('oldName'),
+            newName: $(e.target).val()
+        }
+
+        Meteor.call('pollEditOptionName', pollOption, function(error, result){
+            if(error){
+                return alert(error.reason);
+            };
+        });
+
+        return false;
+
+    },
+
+    'change .newOption': (e)=>{
+        e.preventDefault();
+        
+        let newOption = {
+            _id: Session.get('id'),
+            name: $(e.target).val()
+        };
+        //commit to database
+        Meteor.call('pollAddOption', newOption, (error, result)=>{
+            if(error){
+                return alert(error.reason)
+            }else{
+                $(e.target).closest("input").val('');
+            }
+        });
+        
+        return false;
+
+    },
+    
+    'click .btn-remove': function(e){
+		e.preventDefault();
+
+        let name = $(e.target).closest('.entry').find('.options').val();
+
+        let pollOption = {
+            _id: Session.get('id'),
+            name: name
+        };
+
+        Meteor.call('pollDeleteOption', pollOption, function(error, result){
+            if(error){
+                return alert(error.reason);
+            }
+        });
+        return false;
+	},
+    
+    'submit form': function(e){
+        e.preventDefault();
 
         Router.go('pollDashboard', {_id: this._id})
     }
